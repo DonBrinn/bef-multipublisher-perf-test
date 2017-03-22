e69c1757b732da85cc5494dfddfd093b720a6ec0
Don Brinn <Don.Brinn@D2L.com>
2017-03-22 14:53:26 -0400

    Helper lambda functions to simulate multiple Telegraphs publishing at the same time, for performance testing of a BEF API Gateway

---
+++ src/telegraph-simulator-controller.js
@@ -0,0 +1,56 @@
+const aws = require('aws-sdk');
+
+function invokeTelegraphSimulators(telegraphSimulatorFunctionName, numberOfTelegraphs, telegraphConfig, awsRegion, callback) {
+  const lambda = new aws.Lambda({
+    region: awsRegion
+  });
+
+  var hasError = false;
+  var lambdaErrorHandler = function(error, data) {
+    if (error) {
+      hasError = true;
+      console.log("error invoking a TelegraphSimulator function", error);
+      console.log(data);
+    }
+  }
+
+  var payload = JSON.stringify(telegraphConfig);
+  for (var i = 0; i < numberOfTelegraphs; i++) {
+    lambda.invoke({
+      FunctionName: telegraphSimulatorFunctionName,
+      Payload: payload,
+      InvocationType: "Event" // asynchronous call
+    }, lambdaErrorHandler);
+  }
+  if (!hasError) {
+    callback(null, "invoked " + numberOfTelegraphs + " telegraph simulator functions.");
+  } else {
+    callback('error invoking a TelegraphSimulator function - see log', null);
+  }
+}
+
+exports.handler = (event, context, callback) => {
+  invokeTelegraphSimulators(process.env.TelegraphSimulatorFunctionName, event["number-of-telegraphs"], event["telegraph-config"], process.env.AWS_DEFAULT_REGION, callback);
+};
+
+// Below this line: Used for running locally on your machine
+
+var testInput = {
+  "number-of-telegraphs": 4,
+  "telegraph-config": {
+    "number-of-batches": 2,
+    "wait-between-batches-ms": 100,
+    "bef-gateway-endpoint": "https://qgoew2yfl8.execute-api.us-west-2.amazonaws.com/donb/brightspace",
+    "tenant-id": "71674496-23f5-4457-94ed-ac87c761b817",
+    "auth-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ijk2MmEwZWQ4LTkyYzUtNDZmYy04NTQ3LTMwODE5MjgzNjRmMiJ9.eyJ0ZW5hbnRpZCI6IjcxNjc0NDk2LTIzZjUtNDQ1Ny05NGVkLWFjODdjNzYxYjgxNyIsInNjb3BlIjoiKjoqOioiLCJqdGkiOiJjOWFkYTkyNy02ZmQ2LTQ0ZTEtYmU4ZC0yYWVkNjAxYzYzYTIiLCJpc3MiOiJodHRwczovL2FwaS5icmlnaHRzcGFjZS5jb20vYXV0aCIsImF1ZCI6Imh0dHBzOi8vYXBpLmJyaWdodHNwYWNlLmNvbS9hdXRoL3Rva2VuIiwiZXhwIjoxNDkwMjA4NTI4LCJuYmYiOjE0OTAyMDQ5Mjh9.UO5AjVWtxQmHP9DLAKiMBlGXvlhZ5Dk16A3KZwfP0-ZXerGG128occwzyfLI0J5PPS8kiy2b9181FAO3nBarZQslBI0p_8sdJArY8Uo60n1hL6Ld-mV1snN5MkAzW6siENPhLeAd7tZT8PXmNMFCafegjXcGIkPD8EHBTM0AsGfErDETMJutIyAiI0MwG3_RwNvb-3pse8fJ9WemldJSOlGiJ5Vm2Wmg6orw-WD40Er4uzY-AOjG2i1p45V1sF73JAEjuEebwhMBPQYw0dnaXV3hqAbzs5PLA_xxa6vE_UMh9kjUnZ0VYwzmk3_OZo4-JyLVKPr6ysnZu4wAggKm5Q"
+  }
+};
+
+invokeTelegraphSimulators("bef-multipublisher-perf-test-telegraph-simulator-donb", testInput["number-of-telegraphs"], testInput["telegraph-config"], "us-west-2", (err, result) => {
+  if (err) {
+    console.log("Error:", err);
+    console.log(result);
+  } else {
+    console.log("Success:", result);
+  }
+})
---
+++ src/telegraph-simulator.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const http = require('http');
+const request = require('request-promise-native');
+const oldRequest = require('request'); // so serverless-plugin-include-dependencies will get this necessary indirect dependency
+const uuid = require('node-uuid');
+
+function initAndSimulateTelegraph(input, callback) {
+  simulateTelegraph(input, callback, {}, 0, false, 0)
+}
+
+function simulateTelegraph(input, callback, errorCounts, successCount, hasError, publishCount) {
+  var batchLimit = input["number-of-batches"];
+  if (!batchLimit || batchLimit < 1) {
+    callback("invalid number-of-batches specified", batchLimit);
+    return;
+  }
+  publishMessage(input["bef-gateway-endpoint"], input["auth-token"], input["tenant-id"], errorCategory => {
+    if (errorCategory) {
+      hasError = true;
+      if (errorCounts[errorCategory]) {
+        errorCounts[errorCategory] += 1;
+      } else {
+        errorCounts[errorCategory] = 1;
+      }
+    } else {
+      successCount += 1;
+    }
+  })
+    .then(() => {
+      publishCount += 1;
+      if (publishCount >= batchLimit) {
+        if (hasError) {
+          callback(JSON.stringify({"Number of each error type encountered": errorCounts}), errorCounts);
+        } else {
+          callback(null, successCount);
+        }
+      } else {
+        setTimeout(simulateTelegraph, input["wait-between-batches-ms"], input, callback, errorCounts, successCount, hasError, publishCount);
+      }
+    });
+}
+
+function publishMessage(endpoint, authToken, tenantId, callback) {
+  var payload = {
+    Events: [
+      {
+        EventType: "UserInteractionEvent",
+        EventId: uuid.v4(),
+        TenantId: tenantId
+      }
+    ]
+  };
+  return request.post(
+    endpoint,
+    {
+      json: payload,
+      auth: { bearer: authToken}
+    }
+  )
+  .then(response => {
+    callback(null);
+  })
+  .catch(error => {
+    var errorCategory = null;
+    if (error.statusCode) {
+      errorCategory = error.statusCode;
+    } else {
+      errorCategory = "error making request - see log";
+      console.log("error making request:", error);
+    }
+
+    callback(errorCategory);
+  });
+}
+
+module.exports.handler = (event, context, callback) => {
+  initAndSimulateTelegraph(event, callback);
+};
+
+// Below this line: Used for running locally on your machine
+
+var testInput = {
+  "number-of-batches": 2,
+  "wait-between-batches-ms": 100,
+  "bef-gateway-endpoint": "https://qgoew2yfl8.execute-api.us-west-2.amazonaws.com/donb/brightspace",
+  "tenant-id": "71674496-23f5-4457-94ed-ac87c761b817",
+  "auth-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ijk2MmEwZWQ4LTkyYzUtNDZmYy04NTQ3LTMwODE5MjgzNjRmMiJ9.eyJ0ZW5hbnRpZCI6IjcxNjc0NDk2LTIzZjUtNDQ1Ny05NGVkLWFjODdjNzYxYjgxNyIsInNjb3BlIjoiKjoqOioiLCJqdGkiOiIyM2E2ZDcwNi1lNjdlLTQwMDgtOWU3My02MDNjZDI1MjgxNDkiLCJpc3MiOiJodHRwczovL2FwaS5icmlnaHRzcGFjZS5jb20vYXV0aCIsImF1ZCI6Imh0dHBzOi8vYXBpLmJyaWdodHNwYWNlLmNvbS9hdXRoL3Rva2VuIiwiZXhwIjoxNDkwMTk4NDI1LCJuYmYiOjE0OTAxOTQ4MjV9.YHbHJq1sCXvLrB6twNdmMhY8A_nc-jH78xRLjST9WSeS_g7HjU3VLYGtbWAmrKnTZNmSWzKFINBAof3BAuljORzZLzw-GxhW6i6hKMph-8-LOSah938VglnwjjF2EiNtErhpVzAQMMTzSdBcVSkg6_Bz9NOhKeS6xGh4n5CnGD82cx630OE215fC_2vJCq5KhFj_UGfCqYC9C-OWGxk2vbBt2YM1iX_kIHQ_tTjXy5Kp0pYilfcewX4_Tz8h2XD5U3liH0YKJZ9QwJU1NQvwUpGvjGL-rs_mgSeyHNJmgQ-0EB_2xyibMtqByC1u7pMWYJNmZr3PhVFTZQBHCIWCYQ"
+};
+
+initAndSimulateTelegraph(testInput, (err, response) => {
+  if(err) {
+    console.log("Error:", err);
+    console.log(response);
+  } else {
+     console.log("Success:", response);
+  }
+});
